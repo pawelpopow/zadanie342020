a525f2065ecbda7ab6b040e0fcc0d76c7a4ddc7f
ppopow <pawel.popow@gmail.com>
2020-10-20 13:36:43 +0200

    nowe zadanko34

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let liczba1='JavaScript';
+let liczba2='Jestem świetnym programistą';
+
+function multiply(liczba1, liczba2) {
+    let result = liczba1 * liczba2
+    if (statesUS.length > 50) {
+        statesUS.length=50
+     }
+    }
+
+multiply (liczba1, liczba2);
